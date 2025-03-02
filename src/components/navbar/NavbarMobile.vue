6d7ab39d73f08ec465f4ba94d1367049a7f59796
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2025-03-02 14:59:00 +0200

    fix and improve navbar and footer

---
+++ src/components/navbar/NavbarMobile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="inset-0 fixed" @click="closeMenu"></div>
+  <ul
+    class="md:hidden flex flex-col bg-white p-3 absolute top-full right-0 shadow-lg w-[200px] h-[30vh] text-lg items-center"
+  >
+    <li v-for="(item, index) in navLinks" :key="index">
+      <router-link
+        class="block py-4 px-4 text-[var(--secondary-color)] hover:bg-gray-100"
+        :to="item.path"
+        @click="closeMenu"
+      >
+        {{ item.label }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    navLinks: Array,
+  },
+  methods: {
+    closeMenu() {
+      this.$emit("closeMenu");
+    },
+  },
+};
+</script>
