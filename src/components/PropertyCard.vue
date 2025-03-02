688a64ac86e2079433a91308184a129a4eb9c0bf
mariam168 <159446706+mariam168@users.noreply.github.com>
2025-03-02 16:12:14 +0200

    Add files via upload

---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <div class="bg-white text-gray-800 shadow-lg rounded-lg overflow-hidden ">
+      <img :src="property.image" alt="Property Image" class="w-full h-48 object-cover">
+      <div class="p-4">
+        <h2 class="font-bold text-md">{{ property.title }}</h2>
+        <p class="text-gray-500">{{ property.location }}</p>
+        <p class="font-bold text-sm">{{ property.price }} $</p>
+        <div class="flex justify-between text-gray-600 mt-5">
+          <span><i class="bi bi-buildings-fill pr-1" style="color: var(--secondary-color);" ></i>{{ property.size }} m²</span>
+          <span><i class="bi bi-house-fill pr-1" style="color: var(--secondary-color);"></i>{{ property.rooms }} Rooms</span>
+          <span><i class="bi bi-h-square pr-1" style="color: var(--secondary-color);"></i>{{ property.units }}</span>
+        </div>
+      </div>
+    </div>
+
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      property: Object,
+    },
+  };
+  </script>
+  
